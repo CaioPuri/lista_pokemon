6a2b5c371eba1f16f51e546a8408917c7ac72460
CaioPuri <131062850+CaioPuri@users.noreply.github.com>
2023-05-14 19:43:31 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocadeTema = document.querySelector(".botao-sol");
+
+botaoAlterarTema.addEventListener("click", () => {
+    
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+    
+    if (modoEscuroEstaAtivo) {
+            imagemBotaoTrocadeTema.setAttribute("src", "./src/imagens/sun.png");
+        
+        }else{
+            imagemBotaoTrocadeTema.setAttribute("src", "./src/imagens/moon.png");
+        }
+});
